923a90d4eaa0f52080c06e4e74d70bd5d73b1f76
josedev72 <128483615+josedev72@users.noreply.github.com>
2023-04-22 00:54:08 -0300

    Se actualizó para probar Gana Equipo1 y noGanaEquipo2

--- test/java/com/proyecto/clasesTest/PronosticoTest.java
+++ test/java/com/proyecto/clasesTest/PronosticoTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.Test;
 
 public class PronosticoTest {
     @Test
-    public void probarPuntosPronosticos() {
+    public void probarPuntosPronosticosGanaEquipo1() {
         // Declaramos los valores/datos necesarios
 
         // Constructor Equipo(String nombre, String descripcion)
@@ -25,5 +25,24 @@ public class PronosticoTest {
 
         // probar el metodo puntosPronosticos de la clase Pronostico
         Assertions.assertEquals(1,pronostico.puntosPronosticos());
+    }
+
+    @Test
+    public void probarPuntosPronosticosNoGanaEquipo2() {
+        // Declaramos los valores/datos necesarios
+
+        // Constructor Equipo(String nombre, String descripcion)
+        Equipo equipo1 = new Equipo("Argentina", "el de la 3ra estrella");
+        Equipo equipo2 = new Equipo("Mexico", "");
+
+        // Constructor Partido(int ronda, Equipo equipo1, Equipo equipo2, int golesEquipo1, int golesEquipo2)
+        Partido partido = new Partido(1, equipo1, equipo2, 2, 0);
+        ResultadoEnum resultado = ResultadoEnum.ganador;
+
+        // Constructor Pronostico(Partido partido, Equipo equipo, ResultadoEnum resultado)
+        Pronostico pronostico = new Pronostico(partido, equipo2, resultado);
+
+        // probar el metodo puntosPronosticos de la clase Pronostico
+        Assertions.assertEquals(0,pronostico.puntosPronosticos());
     }
 }
