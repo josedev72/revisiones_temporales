826ab9e887fa657fa0f863a3ad84ad628064c800
josedev72 <128483615+josedev72@users.noreply.github.com>
2023-04-22 00:46:51 -0300

    Se crearon Test para probar Ganador Equipo1 y Perdedor Equipo2

--- test/java/com/proyecto/clasesTest/PartidoTest.java
+++ test/java/com/proyecto/clasesTest/PartidoTest.java
@@ -1,4 +1,44 @@
 package com.proyecto.clasesTest;
 
+import com.proyecto.clases.Equipo;
+import com.proyecto.clases.Partido;
+import com.proyecto.clases.ResultadoEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class PartidoTest {
+    @Test
+    public void probarResultadoGanaEquipo1(){
+        //Datos necesarios
+        Equipo equipo1 = new Equipo("Argentina", "");
+        Equipo equipo2 = new Equipo("Mexico", "");
+        int golesEquipo1, golesEquipo2;
+        golesEquipo1=2;
+        golesEquipo2=0;
+
+        //constructor Partido(int ronda, Equipo equipo1, Equipo equipo2, int golesEquipo1, int golesEquipo2)
+        Partido partido=new Partido(1,equipo1,equipo2,golesEquipo1,golesEquipo2);
+
+        ResultadoEnum resultado = ResultadoEnum.ganador;
+        // probar el metodo resultado de la clase Partido
+        Assertions.assertEquals(resultado, partido.resultado(equipo1));
+    }
+
+    @Test
+    public void probarResultadoPierdeEquipo2(){
+        //Datos necesarios
+        Equipo equipo1 = new Equipo("Argentina", "");
+        Equipo equipo2 = new Equipo("Mexico", "");
+        int golesEquipo1, golesEquipo2;
+        golesEquipo1=2;
+        golesEquipo2=0;
+
+        //constructor Partido(int ronda, Equipo equipo1, Equipo equipo2, int golesEquipo1, int golesEquipo2)
+        Partido partido=new Partido(1, equipo1, equipo2, golesEquipo1, golesEquipo2);
+
+        ResultadoEnum resultado = ResultadoEnum.perdedor;
+        // probar el metodo resultado de la clase Partido
+        Assertions.assertEquals(resultado, partido.resultado(equipo2));
+    }
 }
